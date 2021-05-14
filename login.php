778046c2c48a040107b44bdb12f53fb770d53812
bennyfajri <m.b.fajrii@gmail.com>
2021-05-14 23:46:58 +0700

    fix login and register

--- login.php
+++ login.php
@@ -1,26 +1,35 @@
 <?php
-include "dbconnect.php";
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$response = array();
-	$username = $_POST['username'];
-	$password = md5($_POST['password']);
-
-	// langkah 2
-	$cek = "SELECT * FROM laundry WHERE username='$username' and password='$password'";
-	$result = mysqli_fetch_array(mysqli_query($conn, $cek)); 
-        if (isset($result)) {
-	$response['value'] = 1;
-	$response['id_user'] = $result['ID_User'];
-	$response['message'] = "Login berhasil";
-        $response['username'] = $result['Username'];
-	$response['nama_user'] = $result['Nama_User'];
-	$response['nama_laundry'] = $result['Nama_Laundry'];
-	echo json_encode($response);
-} else {
-	$response['value'] = 0;
-	$response['message'] = "Login gagal"; echo json_encode($response);
-	}
-} 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+include_once("dbconnect.php");
+$postdata = file_get_contents("php://input");
+    if (isset($postdata)) {
+        $request1 = json_decode($postdata);
+        $username = $request1->username;
+        $password = $request1->password;
+        if( $username == '' || $password == '' ){
+            echo json_encode(array( "status" => false,"message" => "Parameter missing!") );
+         }else{
+            $query= "SELECT * FROM laundry WHERE Username='$username' AND Password='$password'";
+                $result= mysqli_query($conn, $query);
+                if(mysqli_num_rows($result) > 0){  
+                 $query= "SELECT * FROM laundry WHERE Username = '$username' AND Password ='$password'";
+                             $result= mysqli_query($conn, $query);
+                         $emparray;
+                             if(mysqli_num_rows($result) > 0){  
+                             while ($row = mysqli_fetch_assoc($result)) {
+                                         $emparray = $row;
+                                       }
+                             }
+                   echo json_encode(array( "status" => true,"message" => "Login successfully!", "data" => $emparray) );
+                }else{ 
+                    echo json_encode(array( "status" => false,"message" => "Invalid email or password!") );
+                }
+                 mysqli_close($conn);
+         }
+    }
+} else{
+    echo json_encode(array( "status" => false,"message" => "Error occured, please try again!") );
+}
 
 ?>
--- register.php
+++ register.php
@@ -1,34 +1,44 @@
 <?php
-include "dbconnect.php";
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-	$response = array();
-	$username = $_POST['username'];
-	$password = md5($_POST['password']);
-        $nama_user = $_POST['nama_user'];
-	$nama_laundry = $_POST['nama_laundry'];
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    include_once("dbconnect.php");
+    $postdata = file_get_contents("php://input");
 
-	//langkah 2
-	$cek = "SELECT * FROM laundry WHERE Username = '$username'";
-	$result = mysqli_fetch_array(mysqli_query($conn, $cek));
+    if (isset($postdata)) {
+        $request1 = json_decode($postdata);
+        $username = $request1->username;
+        $namaLaundry = $request1->namaLaundry;
+        $namaUser = $request1->namaUser;
+        $password = $request1->password;
+         if($username == '' || $namaLaundry == '' || $namaUser == '' || $password == ''){
+                echo json_encode(array( "status" => "false","message" => "Parameter missing!") );
+         }else{
+                $query= "SELECT * FROM laundry WHERE username='$username'";
+                $result= mysqli_query($conn, $query);
+                if(mysqli_num_rows($result) > 0){  
+                   echo json_encode(array( "status" => "false","message" => "Username already exist!") );
+                }else{ 
+                 $query = "INSERT INTO laundry (Username,Password,Nama_User,Nama_Laundry) VALUES ('$username','$password','$namaUser','$namaLaundry')";
+                 if(mysqli_query($conn,$query)){
+                     $query= "SELECT * FROM laundry WHERE username='$username'";
+                             $result= mysqli_query($conn, $query);
+                         $emparray = array();
+                             if(mysqli_num_rows($result) > 0){  
+                             while ($row = mysqli_fetch_assoc($result)) {
+                                         $emparray[] = $row;
+                                         echo json_encode(array( "status" => "true","message" => "Successfully registered!" , "data" => $row) );
+                                       }
+                             }
+                 }else{
+                     echo json_encode(array( "status" => "false","message" => "Error occured, please try again!") );
+                }
+            }
+                    mysqli_close($con);
+         }
+    }
 
-	if(isset($result)){
-		$response['value'] = 2;
-		$response['message'] = "Username telah digunakan";
-
-		echo json_encode($response);
-	} else {
-		$insert = "INSERT INTO laundry VALUE(NULL, '$username','$password',
-		'$nama_user','$nama_laundry')"; 
-
-		if(mysqli_query($conn, $insert)){
-			$response['value'] = 1;
-			$response['message'] = "Berhasil didaftarkan";
-			echo json_encode(($response));
-		}else{
-			$response['value'] = 0; 
-			$response['message'] = "Gagal didaftarkan";
-			echo json_encode($response);
-		}
-	}
+} else{
+    echo json_encode(array( "status" => "false","message" => "Error occured, please try again!") );
 }
+
+ ?>
